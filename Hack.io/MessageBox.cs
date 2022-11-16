529636b85733ca7794a6527cbf45c517c20f84a9
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-11-15 19:21:08 -0600

    Upgrade projects to net6/net7

--- Hack.io/MessageBox.cs
+++ Hack.io/MessageBox.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Text;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode = true)]
 
 namespace Hack.io.Util
 {
